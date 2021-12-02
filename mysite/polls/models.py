5062c61d8f8246cb4c0d3cc52eff55ebd5adb23d
clem-lt <clementine.letohic@orange.fr>
2021-12-02 19:41:24 +0100

    test2

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 import datetime
 from django.utils import timezone
+from datetime import datetime, timedelta
+from django.contrib.auth.models import User
 
 class Client(models.Model):
-    nom = models.CharField(max_length=15)
-    prenom = models.CharField(max_length=15)
-    situation = models.CharField(max_length=15)   #chômeur gratuit étudiant élève ou militaire 
-    solde = models.DecimalField(max_digits=5, decimal_places=2)
-    penalites = models.IntegerField(default=0)
+    user = models.OneToOneField(User, on_delete = models.CASCADE, null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    status = models.CharField(max_length=15, default='plein tarif') # chômeur gratuit étudiant, élève ou militaire
+    bad_borrower = models.BooleanField(default=False)
+    blacklisted_until = models.DateField(null=True, blank=True)
+
+    # nom = models.CharField(max_length=20)
+    # prenom = models.CharField(max_length=20)
+    # status = models.CharField(max_length=15)   #chômeur gratuit étudiant élève ou militaire
+    #solde = models.DecimalField(max_digits=5, decimal_places=2)
+    #penalites = models.IntegerField(default=0)
 
     #liste des emprunts
 
@@ -15,10 +23,35 @@ class Client(models.Model):
         return self.prenom + " " + self.nom
 
 class Livre(models.Model):
-    nomAuteur = models.CharField(max_length=15)
-    prenomAuteur = models.CharField(max_length=15)
+    nomAuteur = models.CharField(max_length=20)
+    prenomAuteur = models.CharField(max_length=20)
     titre = models.CharField(max_length=30)
-    disponibilite=models.BooleanField(default=False)
+    disponible=models.BooleanField(default=True)
+    empruntable=models.BooleanField(default=False)
+    support=models.CharField(null=True, blank=True,max_length=15)
+    categorie=models.CharField(null=True, blank=True,max_length=30)
+    theme=models.CharField(null=True, blank=True,max_length=30)
+    date_publication=models.DateField(null=True, blank=True)
+    cree_le=models.DateField(default=timezone.now())
+    supprime_le=models.DateField(null=True, blank=True)
 
     def __str__(self):
         return self.titre
+
+class Emprunt(models.Model):
+    livre=models.ForeignKey(Livre, on_delete=models.CASCADE)
+    client=models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    emprunte_le=models.DateField(default=timezone.now())
+    retour_max_le=models.DateField(default=timezone.now()+timedelta(days=30))
+    rendu_le=models.DateField(null=True, blank=True)
+    en_retard=models.BooleanField(default=False)
+
+class Paiement(models.Model):
+    client=models.ForeignKey(Client, on_delete=models.CASCADE)
+
+    cree_le=models.DateField(default=timezone.now())
+    deadline=models.DateField(null=True,blank=True)
+    paye_le=models.DateField(null=True, blank=True)
+    raison=models.CharField(max_length=30)
+    montant=models.DecimalField(max_digits=5, decimal_places=2, default=0)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.http import Http404
 from django.shortcuts import render
 from django.contrib.auth import *
 from django.views.generic import TemplateView
+from django.db import connection
 
 from .models import Livre
 from .forms import NameForm
@@ -37,13 +38,26 @@ def search(request):
     }
     return render(request, 'polls/search.html', context)
 
+# def detail(request, pk):
+#     livre = Livre.objects.get(pk=pk)
+#     context = {
+#         'livre_titre': livre.titre,
+#         'livre_nomAuteur': livre.nomAuteur,
+#         'livre_prenomAuteur': livre.prenomAuteur,
+#         'livre_disponibilite': livre.disponible
+#     }
+#     return render(request, 'polls/detailLivre.html', context)
+
 def detail(request, pk):
-    livre = Livre.objects.get(pk=pk)
+    with connection.cursor() as cursor :
+        cursor.execute("SELECT * FROM polls_Livre WHERE id=%s", [pk])
+        columns = [col[0] for col in cursor.description]
+        livre = [dict(zip(columns, row)) for row in cursor.fetchall()]
     context = {
-        'livre_titre': livre.titre,
-        'livre_nomAuteur': livre.nomAuteur,
-        'livre_prenomAuteur': livre.prenomAuteur,
-        'livre_disponibilite': livre.disponibilite
+        'livre_titre': livre[0]["titre"],
+        'livre_nomAuteur': livre[0]["nomAuteur"],
+        'livre_prenomAuteur': livre[0]["prenomAuteur"],
+        'livre_disponibilite': livre[0]["disponible"]
     }
     return render(request, 'polls/detailLivre.html', context)
 
@@ -83,3 +97,13 @@ def get_name(request):
 
 class DashboardView(TemplateView):
     template_name = "polls/dashboard.html"
+
+
+
+## Fontions SQL
+
+def infos_perso(self):
+    with connection.cursor() as cursor :
+        cursor.execute("SELECT * FROM Client where username=%s", [self.user])
+        row = cursor.fetchone()
+    return row
