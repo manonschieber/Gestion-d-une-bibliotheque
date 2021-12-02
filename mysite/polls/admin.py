a425329032b3167f922553a98aeef14af2493496
clem-lt <clementine.letohic@orange.fr>
2021-12-02 19:43:21 +0100

    test3

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-from .models import Client, Livre
 
-admin.site.register(Client)
+from .models import *
+
+
 admin.site.register(Livre)
+admin.site.register(Emprunt)
+admin.site.register(Paiement)
+
+class ClientInline(admin.StackedInline):
+    model=Client
+    can_delete=False
+    verbose_name_plural='client'
+
+class UserAdmin(BaseUserAdmin):
+    inlines=(ClientInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+
+
